9f1ffb831a5a7a3e0a044aaf52ab2b2700320cb8
Alexander Sturz (_61106960_) <alex@sturz.org>
2021-10-02 12:53:44 +0200

    version 0.7.2

--- adPEAS.ps1
+++ adPEAS.ps1
@@ -143,7 +143,7 @@ Start adPEAS, enumerate the domain 'contoso.com' and use the module 'Bloodhound'
 
     <# +++++ Starting adPEAS +++++ #>
     Write-Host ''
-    $adPEASVersion = '0.7.1'
+    $adPEASVersion = '0.7.2'
     Invoke-Logger -LogClass Info -LogValue "+++++ Starting adPEAS Version $adPEASVersion +++++"
     "adPEAS version $adPEASVersion"
 
