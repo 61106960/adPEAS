c9aed568b1057ba8b163bd216d416a5b9d6669b4
61106960 <alexander@sturz.org>
2023-11-20 17:07:00 +0100

    version 0.8.19

--- adPEAS-Light.ps1
+++ adPEAS-Light.ps1
@@ -149,7 +149,7 @@ Start adPEAS, enumerate the domain 'contoso.com' and use the module 'Bloodhound'
 
     <# +++++ Starting adPEAS +++++ #>
     $ErrorActionPreference = "Continue"
-    $adPEASVersion = '0.8.18'
+    $adPEASVersion = '0.8.19'
 
     # Check if outputfile is writable and set color
     if ($PSBoundParameters['Outputfile']) {
